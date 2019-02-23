952c175c12fa44b06381b27bd1500ef2d0971286
Ari <aristath@gmail.com>
2019-02-24 01:13:36 +0200

    add kirki.control script

--- src/assets/scripts/control.js
+++ src/assets/scripts/control.js
@@ -34,3 +34,54 @@ kirki.input.select = {
         } );
     }
 };
+
+
+kirki.control = kirki.control || {};
+kirki.control['kirki-select'] = {
+
+    /**
+     * Init the control.
+     *
+     * @since 3.0.17
+     * @param {Object} control - The customizer control object.
+     * @returns {null}
+     */
+    init: function( control ) {
+        var self = this;
+
+        // Render the template.
+        self.template( control );
+
+        // Init the control.
+        kirki.input.select.init( control );
+    },
+
+    /**
+     * Render the template.
+     *
+     * @since 3.0.17
+     * @param {Object}  control - The customizer control object.
+     * @param {Object}  control.params - The control parameters.
+     * @param {string}  control.params.label - The control label.
+     * @param {string}  control.params.description - The control description.
+     * @param {string}  control.params.inputAttrs - extra input arguments.
+     * @param {Object}  control.params.default - The default value.
+     * @param {Object}  control.params.choices - The choices for the select dropdown.
+     * @param {string}  control.id - The setting.
+     * @returns {null}
+     */
+    template: function( control ) {
+        var template = wp.template( 'kirki-input-select' );
+
+        control.container.html( template( {
+            label: control.params.label,
+            description: control.params.description,
+            'data-id': control.id,
+            inputAttrs: control.params.inputAttrs,
+            choices: control.params.choices,
+            value: kirki.setting.get( control.id ),
+            multiple: control.params.multiple || 1,
+            placeholder: control.params.placeholder
+        } ) );
+    }
+};
