a545e9189a64b12132a7511e0681b40a34a7b6a0
Ari <aristath@gmail.com>
2019-04-20 14:54:04 +0300

    allow adding data to select2

--- src/assets/scripts/control.js
+++ src/assets/scripts/control.js
@@ -17,10 +17,17 @@ wp.customize.controlConstructor['kirki-select'] = wp.customize.kirkiDynamicContr
         if ( 1 < multiple ) {
             selectWooOptions.maximumSelectionLength = multiple;
         }
+
+        if ( control.params.select_args ) {
+            selectWooOptions = jQuery.extend( selectWooOptions, control.params.select_args );
+            if ( 'string' === typeof selectWooOptions.data && 'object' === typeof window[ selectWooOptions.data ] ) {
+                selectWooOptions.data = window[ selectWooOptions.data ];
+            }
+        }
         jQuery( element ).selectWoo( selectWooOptions ).on( 'change', function() {
             selectValue = jQuery( this ).val();
             selectValue = ( null === selectValue && 1 < multiple ) ? [] : selectValue;
             control.setting.set( selectValue );
-        } );
+        });
     }
 });
